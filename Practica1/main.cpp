78963d78ec732887b7ef29ebd16f4c3ea89a7ca2
Osman Mehmed <osman.mehmed@edu.uah.es>
2019-11-01 15:23:59 +0100

    Implementado hasta el punto 3 (inclusive) de la parte de Implementación de https://learn-eu-central-1-prod-fleet01-xythos.s3-eu-central-1.amazonaws.com/5d1f8301cf6a3/1810554?response-content-disposition=inline%3B%20filename%2A%3DUTF-8%27%27PECL1_EEDD_2019_20.pdf&response-content-type=application%2Fpdf&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20191101T095630Z&X-Amz-SignedHeaders=host&X-Amz-Expires=21600&X-Amz-Credential=AKIAIZ3QX2YUHH4EOO3A%2F20191101%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Signature=8dd65eef12ce2f40ae96ff699892201cd405422ff28e638ab47a1c43ff61d3e0

--- Practica1/main.cpp
+++ Practica1/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <cstdlib>
+#include <ctime>
 #include <string>
 #include "cola.h"
 #include "Pila.h"
@@ -8,17 +9,45 @@
 
 using namespace std;
 
+int random(int desde, int hasta)
+{
+    int outPut;
+
+    srand(time(NULL));
+
+    for (int i = 1; i < 5; i++) {
+    outPut = rand()%((hasta - desde) + 1);
+    if (outPut == 0){
+        outPut++;
+    }
+    }
+    return outPut;
+}
+
 int main()
 {
+
     bool acabado = false;
     string aCode = "0001A";
     string bCode = "0001B";
 
-    int x = rand();
-    int y = rand();
-    int z = rand();
-    int n1 = x%4+2;
-    int n2 = y%4+2;
+    typedef enum estado{OrillaA, OrillaB, NavegandoAB, NavegandoBA};
+    estado estadoBarco = OrillaB;
+
+    string estado1 = "OrillaA";
+    string estado2 = "OrillaB";
+    string estado3 = "NavegandoAB";
+    string estado4 = "NavegandoBA";
+
+    int n1 = random(2,6);
+
+    int n3 = random(80,120)+80;
+    int n2 = random(2,6)+1;
+
+    cout << n1 << endl;
+    cout << n2 << endl;
+    cout << n3 << endl;
+
     Coche* Orilla1[n1];
     Coche* Orilla2[n2];
 
